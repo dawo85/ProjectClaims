fix bug: loaded claims. dinamic calculation size of panel

projectclaims/claims/entities/panel.py
@@ -1,15 +1,22 @@
 import numpy as np
 from claims.entities.claim import Claim
+from django.db.models import F, Max
 
 class Panel():
 
-	width = Claim.max_inch
-	height = Claim.max_inch
-
 	def __init__(self):
-		self.map = np.zeros((self.__class__.width, self.__class__.height))
 		self.claims = Claim.objects.all()
+		self.height = self._get_height_max()
+		self.width = self._get_width_max()
+		self.map = np.zeros((self.height, self.width))
 
+	def _get_height_max(self):
+		height = Claim.objects.aggregate(max_height=Max(F('y') + F('height')))['max_height']
+		return height
+
+	def _get_width_max(self):
+		width = Claim.objects.aggregate(max_width=Max(F('x') + F('width')))['max_width']
+		return width
 
 	def _get_coordinates(self, claim):
 		x_initial = claim.y
@@ -19,7 +26,7 @@ class Panel():
 		return x_initial, y_initial, x_final, y_final
 
 	def put_claim(self, claim):
-		copy_map = np.zeros((self.__class__.width, self.__class__.height))
+		copy_map = np.zeros((self.height, self.width))
 		area = claim.get_area()
 		x_initial, y_initial, x_final, y_final = self._get_coordinates(claim)
 		copy_map[x_initial:y_initial, x_final:y_final] = area

projectclaims/claims/tests/test_fill_panel.py
@@ -9,7 +9,7 @@ class TestFillPanel(APITestCase):
 		self.claim_a = ClaimFactory(x=3, y=1, width=4, height=4)
 		self.claim_b = ClaimFactory(x=1, y=3, width=4, height=4)
 		self.claim_c = ClaimFactory(x=5, y=5, width=2, height=2)
-		self.response = np.zeros((1000, 1000))
+		self.response = np.zeros((7, 7))
 		self.response[1][3]=1
 		self.response[1][4]=1
 		self.response[1][5]=1

projectclaims/claims/tests/test_put_claim_panel.py
@@ -6,24 +6,32 @@ import numpy as np
 class TestPutClaimPanel(APITestCase):
 
 	def setUp(self):
-		self.claim_a = ClaimFactory(x=1, y=2, width=3, height=2)
-		self.response_a = np.zeros((1000, 1000))
-		self.response_a[2][1] = 1
-		self.response_a[3][1] = 1
-		self.response_a[2][2] = 1
-		self.response_a[3][2] = 1
-		self.response_a[2][3] = 1
-		self.response_a[3][3] = 1
-		self.claim_b = ClaimFactory(x=100, y=5, width=1, height=5)
-		self.response_b = np.zeros((1000, 1000))
-		self.response_b[5][100] = 1
-		self.response_b[6][100] = 1
-		self.response_b[7][100] = 1
-		self.response_b[8][100] = 1
-		self.response_b[9][100] = 1
+		self.claim = ClaimFactory(x=3, y=2, width=5, height=4)
+		self.response = np.zeros((6, 8))
+		self.response[2][3] = 1
+		self.response[2][4] = 1
+		self.response[2][5] = 1
+		self.response[2][6] = 1
+		self.response[2][7] = 1
+
+		self.response[3][3] = 1
+		self.response[3][4] = 1
+		self.response[3][5] = 1
+		self.response[3][6] = 1
+		self.response[3][7] = 1
+
+		self.response[4][3] = 1
+		self.response[4][4] = 1
+		self.response[4][5] = 1
+		self.response[4][6] = 1
+		self.response[4][7] = 1
+
+		self.response[5][3] = 1
+		self.response[5][4] = 1
+		self.response[5][5] = 1
+		self.response[5][6] = 1
+		self.response[5][7] = 1
 
 	def test_put_claim_panel(self):
-		panel_map = Panel().put_claim(self.claim_a)
-		self.assertTrue(np.array_equal(self.response_a, panel_map))
-		panel_map = Panel().put_claim(self.claim_b)
-		self.assertTrue(np.array_equal(self.response_b, panel_map))
+		panel_map = Panel().put_claim(self.claim)
+		self.assertTrue(np.array_equal(self.response, panel_map))